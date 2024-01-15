4bb9bb13916b2ee7ac0160d8daf6a9e3db813ab2
IvanL <antila3567@gmail.com>
2024-01-15 21:29:12 +0200

    file manager file

--- CryptoProfilerIl/Services/CoinImageService.swift
+++ CryptoProfilerIl/Services/CoinImageService.swift
@@ -14,16 +14,27 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: Coin
+    private let fileManager = LocalFileManager.intstance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: Coin) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let storedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = storedImage
+        } else {
+            downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image)
         else { return }
-        
         
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
@@ -31,8 +42,11 @@ class CoinImageService {
             })
             .sink { (completion) in NetworkingManager.handleCompletion(data: completion) }
             receiveValue: { [weak self] image in
-                self?.image = image
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = image else { return }
+                
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             }
     }
 }
